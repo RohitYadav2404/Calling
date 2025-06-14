eabcfc74511bd0303adeaabc4b8acc4414a1ff85
RohitYadav2404 <rohit7986477@gmail.com>
2025-06-14 13:10:20 +0530

    env

--- generate-token.js
+++ generate-token.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 // 🔒 Replace these with your actual Twilio credentials
 const ACCOUNT_SID = 'ACeb619479c58f132b11ecf2d17dd8aca9'; // Your Twilio Account SID
-const API_KEY_SID = 'SKcb0748651f295d081dc15e4904d2d292'; // Your Twilio API Key SID
-const API_KEY_SECRET = 'hcU4AVhzxJwkrYFTNFRrp5zjdjT9jCzT';           // Your Twilio API Key Secret
+const API_KEY_SID = 'SKc780f67f965c4d226da2f13e3ff98256'; // Your Twilio API Key SID
+const API_KEY_SECRET = 'dcJ91HWkcgAcf6BMrX9SleDT2mp35x7G';           // Your Twilio API Key Secret
 const TWIML_APP_SID = 'AP862c21b14ccb74756418219f8826152b'; // Your TwiML App SID
 
 const token = jwt.sign(
